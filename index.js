f5e77db38f9da8dde51eef8ca0d77c00a624ba44
JunshuXin <jxin@ucsd.edu>
2025-02-07 21:24:57 -0800

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { fetchJSON, renderProjects, fetchGitHubData } from 'js/global.js';
 async function loadLatestProjects() {
     const projects = await fetchJSON('./lib/projects.json');
     const latestProjects = projects.slice(0, 3);
+    
     const projectsContainer = document.querySelector('.projects');
 
     if (projectsContainer) {
